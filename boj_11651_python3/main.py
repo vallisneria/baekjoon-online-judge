521ddb1e2ac4e4d392a141ffa41d7f19ff0a6668
vallisneria <moogi3469@gmail.com>
2021-07-26 18:15:04 +0900

    boj_11651 <좌표 정렬하기 2> 제출

---
+++ boj_11651_python3/main.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    dot_count = int(input())
+    dots = []
+
+    for _ in range(dot_count):
+        x, y = [int(x) for x in input().split()]
+        dots.append((x, y))
+
+    dots.sort(key=lambda x: (x[1], x[0]))
+
+    for dot in dots:
+        print(dot[0], dot[1])
